2050913d8fb9b94cd9977ff42cbc04a6c0d0861a
mesalm <mahamdabomesalm987@gmail.com>
2024-03-03 19:31:36 +0200

    Add project files.

---
+++ lern c#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //coment
+
+
+            int eag;
+            
+
+        }
+    }
+}
